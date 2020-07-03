2c44f216253cb9bf5a30cc5940e92c338b1cf822
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2020-07-03 08:55:44 +0200

    create user trainings fragment

--- app/app/src/main/java/com/example/streetapp/MainActivity.kt
+++ app/app/src/main/java/com/example/streetapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.streetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.streetapp.fragments.UserTrainings
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, UserTrainings())
+        fragmentTransaction.commit()
     }
 }
---
+++ app/app/src/main/java/com/example/streetapp/fragments/UserTrainingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.streetapp.fragments
+
+import androidx.lifecycle.ViewModel
+
+class UserTrainingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
