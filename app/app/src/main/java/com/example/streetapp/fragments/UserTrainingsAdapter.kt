fd091a1af90ac59ed84c05b6e93a202c65118125
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2020-07-03 15:24:46 +0200

    add data binding to recycler view

--- app/app/src/main/java/com/example/streetapp/fragments/UserTrainingsAdapter.kt
+++ app/app/src/main/java/com/example/streetapp/fragments/UserTrainingsAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.streetapp.R
+import com.example.streetapp.databinding.UserTrainingsItemBinding
 import com.example.streetapp.models.Training
 import java.text.SimpleDateFormat
 
@@ -23,15 +24,28 @@ class UserTrainingsAdapter(val context: Activity,  var trainings: ArrayList<Trai
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserTrainingsHolder {
-        val rootView = LayoutInflater.from(context).inflate(R.layout.user_trainings_item, parent, false)
-        return UserTrainingsHolder(rootView)
+        //val rootView = LayoutInflater.from(context).inflate(R.layout.user_trainings_item, parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = UserTrainingsItemBinding.inflate(inflater)
+        return UserTrainingsHolder(binding)
     }
 
-    inner class UserTrainingsHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
-        var name: TextView = itemView.findViewById<TextView>(R.id.training_name)
-        var type = itemView.findViewById<TextView>(R.id.training_type)
-        var createDate = itemView.findViewById<TextView>(R.id.training_date)
-        var timeInMinutes = itemView.findViewById<TextView>(R.id.timeTextView)
+    inner class UserTrainingsHolder(val binding : UserTrainingsItemBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
+        fun bind(training: Training) {
+            binding.trainingName.text = training.name
+            binding.trainingType.text = training.type
+            val pattern = "yyyy/MM/dd"
+            val simpleDateFormat = SimpleDateFormat(pattern)
+            val date: String = simpleDateFormat.format(training.creatingDate)
+            binding.trainingDate.text = date
+
+            val time = training.timeInMinutes.toString() + "min"
+            binding.timeTextView.text = time
+            /*var name: TextView = itemView.findViewById<TextView>(R.id.training_name)
+            var type = itemView.findViewById<TextView>(R.id.training_type)
+            var createDate = itemView.findViewById<TextView>(R.id.training_date)
+            var timeInMinutes = itemView.findViewById<TextView>(R.id.timeTextView)*/
+        }
         init {
             itemView.setOnClickListener(this)
         }
@@ -48,7 +62,8 @@ class UserTrainingsAdapter(val context: Activity,  var trainings: ArrayList<Trai
 
     override fun onBindViewHolder(holder: UserTrainingsHolder, position: Int) {
         val training = trainings[position]
-        holder.name.text = training.name
+        holder.bind(training)
+        /*holder.name.text = training.name
         holder.type.text = training.type
         val time = training.timeInMinutes.toString() + "min"
         holder.timeInMinutes.text = time
@@ -57,7 +72,7 @@ class UserTrainingsAdapter(val context: Activity,  var trainings: ArrayList<Trai
         val pattern = "yyyy/MM/dd"
         val simpleDateFormat = SimpleDateFormat(pattern)
         val date: String = simpleDateFormat.format(training.creatingDate)
-        holder.createDate.text = date
+        holder.createDate.text = date*/
 
     }
 }
