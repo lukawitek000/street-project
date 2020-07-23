cc2595e909ce9eacc7695cf1dc8bad8ea63fcdae
Łukasz <witkowski553@gmail.com>
2020-07-23 15:51:10 +0200

    add dark theme

--- app/app/src/main/java/com/example/streetapp/fragments/settings/SettingsFragment.kt
+++ app/app/src/main/java/com/example/streetapp/fragments/settings/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.example.streetapp.fragments.settings
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
-import androidx.preference.ListPreference
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 import com.example.streetapp.MainActivity
 import com.example.streetapp.R
 import java.util.*
@@ -41,13 +40,37 @@ class SettingsFragment : PreferenceFragmentCompat() {
             (activity as MainActivity).updateLanguage(newValue.toString())
             val infoToast = resources.getString(R.string.language_changed)
             Toast.makeText(requireContext(), infoToast, Toast.LENGTH_SHORT).show()
-            findNavController().navigateUp()
-            findNavController().navigate(R.id.action_user_trainings_to_settingsFragment)
+            refreshFragment()
+            //(activity as AppCompatActivity).recreate()
+            true
+        }
+
+
+        val themePreference = findPreference<SwitchPreferenceCompat>("theme")
+        themePreference?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener {
+            preference, newValue ->
+            Log.i("SettingsFragment", "newValue $newValue")
+            Log.i("SettingsFragment", "preference key ${preference.key}")
+            Log.i("SettingsFragment", "preference summary ${preference.summary}")
+            //(activity as MainActivity).updateTheme(newValue as Boolean)
+            (activity as AppCompatActivity).recreate()
+            /*
+            val currentActivity = activity as AppCompatActivity
+            currentActivity.finish()
+            currentActivity.overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            currentActivity.startActivity(currentActivity.intent)
+*/
+            //refreshFragment()
             true
         }
 
 
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun refreshFragment(){
+        findNavController().navigateUp()
+        findNavController().navigate(R.id.action_user_trainings_to_settingsFragment)
     }
 
 }
