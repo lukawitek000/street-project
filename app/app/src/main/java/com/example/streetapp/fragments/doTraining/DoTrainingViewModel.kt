2c673c447bd47782f48447707b559250bbc61fae
Łukasz <witkowski553@gmail.com>
2020-07-27 15:09:38 +0200

    make a timer with tutorial

--- app/app/src/main/java/com/example/streetapp/fragments/doTraining/DoTrainingViewModel.kt
+++ app/app/src/main/java/com/example/streetapp/fragments/doTraining/DoTrainingViewModel.kt
@@ -1,9 +1,37 @@
-package com.example.streetapp.fragments
+package com.example.streetapp.fragments.doTraining
 
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import com.example.streetapp.models.Exercise
 import com.example.streetapp.models.Training
 
 class DoTrainingViewModel(val training: Training) : ViewModel() {
+
+
+    var currentExerciseIndex: Int = 0
+    var currentExercise: Exercise = training.exercises[currentExerciseIndex]
+
+
+
+
+
+    fun nextExercise() {
+        if(currentExerciseIndex < training.exercises.size - 1) {
+            currentExercise = training.exercises[++currentExerciseIndex]
+        }
+    }
+
+    fun isLastExercise(): Boolean {
+        return currentExerciseIndex == training.exercises.size-1
+    }
+
+
+
+
+
+
+
 
 
 }
